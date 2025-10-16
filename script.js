326afddeb76370e2b54fc133bea26b1a32391a91
johnmanueldelrosario <johndelrosario123@gmail.com>
2025-10-16 22:52:31 +0800

    October 16, 2025

--- script.js
+++ script.js
@@ -7,9 +7,9 @@
 /* ----------------- CONFIG: Replace these 101 entries ----------------- */
 const images = [
   // Example entries (you MUST replace these with your 101 actual image paths)
-  { src: "mygirl/1.jpg", note: "10/14/25 To my dearest, Riri, I just wanted you to know that I had a tiring day. I keep thinking about you while im in class, I really miss you, I want to hear your voice to cleanses my fatigue. I love you always Yours truly, Jm" },
+  { src: "mygirl/1.jpg", note: "10/14/25 To my dearest, Riri, I just wanted you to know that I had a tiring day. I keep thinking about you while im in class, I really miss you, I want to hear your voice to cleanse my fatigue. I love you always Yours truly, Jm" },
   { src: "mygirl/2.jpg", note: "10/15/25 To my dearest, Riri, Hi again, I miss you so much my babi, today we discussed about sa capstone namin kasi mahihirapan kami sa gamification and after ng class pumunta kami sm para mag ktv, I sang a song for youuuu hehe I love you always Yours truly, Jm" },
-  { src: "mygirl/3.jpg", note: "You said you didn't like chem but loved teaching, I can definitely see it in the future" },
+  { src: "mygirl/3.jpg", note: "10/14/25 To my dearest, Dumplings,Hiiii, Na late ako kanina sa first subject huhu may summative pa naman kami pero buti umabot ako hehehe, grabe yung linakad namin from 9th floor- basement tapos balik ulit sa 9th floor tas ayun wala din class kasi walang computer sa room eh laboratory yung class tapos nung uwian na ktv sana ulit kami pero sarado hehhe ayun lang. I love you alwaysss.  Yours truly, Jm" },
   { src: "mygirl/4.jpg", note: "One of your favorite poses, which suites you. You look cute TT" },
   { src: "mygirl/5.jpg", note: "That smile of yours captivates me" },
   { src: "mygirl/6.jpg", note: "The shape of your eyes that oddly fits your glasses" },
